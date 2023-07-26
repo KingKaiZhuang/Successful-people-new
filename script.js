6f685d93f6313b7160a2733ded52abe67b151a63
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-07-26 22:31:40 +0800

    RWD

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('#product').owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplaySpeed: 500,
+        nav: false,
+        dots: false
+    })
+})
