c6113bfc857b38ad353aaa7c0529cb44e6911346
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-07-27 14:35:39 +0800

    RWD

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ $(function () {
         dots: false
     })
 })
+// nav 
+let toggles = document.querySelectorAll('.nav-toggle');
+let menu = document.querySelector('.menu-group');
+
+for (toggle of toggles) {
+    toggle.onclick = e => {
+        menu.classList.toggle('active');
+        if (menu.style.height) {
+            menu.style.height = '';
+        } else {
+            menu.style.height = menu.scrollHeight + 'px';
+        }
+    };
+}
+
+window.onresize = e => {
+    let windowWidth = window.innerWidth;
+    let menu = document.querySelector('.menu-group');
+    if (windowWidth > 720) {
+        menu.style.height = 'initial';
+        menu.style.overflow = 'initial';
+    } else {
+        menu.style.height = menu.scrollHeight + 'px';
+        menu.style.overflow = 'hidden';
+    }
+}
